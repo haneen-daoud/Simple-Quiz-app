12591d5c450051b273898f9d38cb19b130245836
haneen-daoud <haneen2592002@gmail.com>
2024-09-17 12:34:48 +0300

    add viewAnswer btn

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,7 +19,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int quastionIndex = 0;
   int score = 0;
   String selectedAnswer = '';
-  bool isclick=false;
+  bool isclick = false;
+  bool viewAnswer = false;
 
   //we made a widget to reduce the duplication in code and enhance performance haneen
   Widget answerButton({required String text, required VoidCallback onPressed}) {
@@ -45,89 +46,98 @@ class _MyHomePageState extends State<MyHomePage> {
           child: !isFinished
               ? Column(
                   children: [
-                    
-                      const SizedBox(
-                        height: 8,
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      quastionWithAnswers[quastionIndex].quastion,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic,
                       ),
-                      Text(
-                        quastionWithAnswers[quastionIndex].quastion,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                        ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      'answer and get point ',
+                      style: TextStyle(
+                        color: AppColors.grey,
+                        fontSize: 12,
                       ),
-                      const SizedBox(
-                        height: 8,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                        'step ${quastionIndex + 1} to ${quastionWithAnswers.length}'),
+                    Column(
+                      children: quastionWithAnswers[quastionIndex]
+                          .answers
+                          .map((answer) => AnswerWidgetItem(
+                              answer: answer,
+                              selectedAnswer: selectedAnswer,
+                              onTap: () {
+                                setState(() {
+                                  selectedAnswer = answer.text;
+                                  isclick = true;
+                                });
+                              }))
+                          .toList(),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.pink,
+                        disabledForegroundColor: Colors.grey.withOpacity(0.38),
                       ),
-                      const Text(
-                        'answer and get point ',
-                        style: TextStyle(
-                          color: AppColors.grey,
-                          fontSize: 12,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text('step ${quastionIndex+1} to ${quastionWithAnswers.length}'),
-                      Column(
-                        children:
-                         quastionWithAnswers[quastionIndex]
-                            .answers
-                            .map((answer) => AnswerWidgetItem(
-                                answer: answer,
-                                selectedAnswer: selectedAnswer,
-                                onTap: () {
-                                  setState(() {
-                                    selectedAnswer = answer.text;
-                                    isclick=true;
-                                  });
-                                }))
-                            .toList(),
-                      ),
-                      const Spacer(),
-                    MainButton( text :'Next',
-                    onTap:(){
-                       setState(() {
-                              if (selectedAnswer ==
-                                  quastionWithAnswers[quastionIndex]
-                                      .correctAnswer) {
-                                score++;
-                              }
+                      onPressed: isclick?() {
+                        setState(() {
+                          if(isclick){
+                          viewAnswer = true;
+                          }
+                          else{
+                            viewAnswer=false;
+                          }
+                        });
+                      }:null,
+                      child: const Text('show correct answer'),
+                    ),
+                    if (viewAnswer&&isclick)
+                      Text(quastionWithAnswers[quastionIndex].correctAnswer),
 
-                              if (quastionIndex <
-                                  quastionWithAnswers.length - 1 ) {
+                    const Spacer(),
+                    MainButton(
+                        text: 'Next',
+                        onTap: () {
+                          setState(() {
+                            if (selectedAnswer ==
+                                quastionWithAnswers[quastionIndex]
+                                    .correctAnswer) {
+                              score++;
+                            }
 
-
-                                if(isclick){
+                            if (quastionIndex <
+                                quastionWithAnswers.length - 1) {
+                              if (isclick) {
                                 quastionIndex++;
-                                isclick=false;
-
-                                }
-                                else{
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    
-                                    const SnackBar(
-                                  content:  Text('You should select an answer before proceeding to the next question.')
-
-                                    ),
-                                    
-                                    );
-
-                                }
-                              } else{
-                                isFinished = true;
+                                isclick = false;
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text(
+                                          'You should select an answer before proceeding to the next question.')),
+                                );
                               }
-                                
-                              
-                              
 
-
-                            });
-                    
-                    }) ,
-                    
+                              
+                                viewAnswer = false;
+                              
+                            } else {
+                              isFinished = true;
+                            }
+                          });
+                        }),
                     const SizedBox(height: 8),
                     TextButton(
                         onPressed: () {
